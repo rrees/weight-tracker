e949b51d8f17a6af7f8c076b60cca894417df05c
Robert Rees <shudderfix@gmail.com>
2023-08-13 10:55:19 +0100

    Sets up the app to run on Fly

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn app.app:app
+# Modify this Procfile to fit your needs
+web: gunicorn --bind 0.0.0.0:8080 app.app:app
--- app/app.py
+++ app/app.py
@@ -4,15 +4,13 @@ import sys
 
 import flask
 
-from flask_sslify import SSLify
-
 from . import handlers
 from . import redis_utils
 
 ENV = os.environ.get("ENV", "PROD")
 
 logging.basicConfig(
-    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     level=logging.DEBUG if not ENV == "PROD" else logging.INFO,
     stream=sys.stderr,
 )
@@ -24,22 +22,20 @@ redis = redis_utils.setup_redis(redis_url) if redis_url else None
 app = flask.Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", os.urandom(24))
 
-if not ENV == "DEV":
-    sslify = SSLify(app)
-
 logger = app.logger
 
 routes = [
-	('/', 'index', handlers.pages.front_page, ['GET']),
-	('/home', 'home', handlers.pages.home, ['GET']),
-	('/forms/weight-log', 'log_weight', handlers.forms.weight_log, ['POST']),
+    ("/", "index", handlers.pages.front_page, ["GET"]),
+    ("/home", "home", handlers.pages.home, ["GET"]),
+    ("/forms/weight-log", "log_weight", handlers.forms.weight_log, ["POST"]),
 ]
 
 for path, endpoint, handler, methods in routes:
-	app.add_url_rule(path, endpoint, handler, methods=methods)
+    app.add_url_rule(path, endpoint, handler, methods=methods)
+
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
+    logging.exception("An error occurred during a request.")
+    return "An internal error occurred.", 500
