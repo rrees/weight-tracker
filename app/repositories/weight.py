f98644f480014a6da5eb8e369cfd8340bfae4f23
Robert Rees <shudderfix@gmail.com>
2024-02-10 17:25:41 +0000

    Lookup the database environment on every call to avoid cached values

--- app/repositories/weight.py
+++ app/repositories/weight.py
@@ -6,24 +6,26 @@ from psycopg.rows import dict_row
 
 from . import sql
 
-_db_url = os.environ["DATABASE_URL"]
+
+def _db_url():
+    return os.environ["DATABASE_URL"]
 
 
 def today(weight_in_grams):
-    with psycopg.connect(_db_url) as conn:
+    with psycopg.connect(_db_url()) as conn:
         with conn.cursor() as cursor:
             cursor.execute(sql.insert_today, {"weight_in_grams": weight_in_grams})
 
 
 def history():
-    with psycopg.connect(_db_url) as conn:
+    with psycopg.connect(_db_url()) as conn:
         with conn.cursor() as cursor:
             results = cursor.execute(sql.history)
             return [r for r in results]
 
 
 def recent_history():
-    with psycopg.connect(_db_url, row_factory=dict_row) as conn:
+    with psycopg.connect(_db_url(), row_factory=dict_row) as conn:
         with conn.cursor() as cursor:
             results = cursor.execute(sql.recent_history)
             return [r for r in results]
